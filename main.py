348a628e5122380f67b9351331f21a6919e48c89
Lucas Mahauni <lucasraoniha@gmail.com>
2022-06-04 23:34:28 -0300

    version1.1 has ability to take multiple photos

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ import numpy as np
 import os
 import requests
 import imutils
+import threading
+import time
 #import urllib.request
 
+#url from the IP webcam
+URL = "http://192.168.0.15:8080/shot.jpg"
 
-#not tested this version, only with the non alteration one. (the commented one)
-def main():
-    #url from the IP webcam
-    url = "http://192.168.0.15:8080/shot.jpg"
-
-    #Save image with resolution alterations with it (didn't test it yet)
-    #can delete this part if want to save the image from url
-    img_resp = requests.get(url)
+def getImage():
+    img_resp = requests.get(URL)
     img_arr = np.array(bytearray(img_resp.content), dtype = np.uint8)
     img = cv2.imdecode(img_arr, -1)
     img = imutils.resize(img, width=1000, height=1800)
+    return img
 
+def thread_function(i):
+    for j in range(0, i):
+        pic = getImage()
+        cv2.imwrite("./images/pic"+str(j)+".jpg", pic)
+        time.sleep(1)
+
+#not tested this version, only with the non alteration one. (the commented one)
+def main():
     #create the directory images
     if not os.path.exists("images"):
         os.mkdir("images")
 
-    #Need to test if will make it work
-    savePhoto(img)
-
-    #The full command tested and worked
-    #print(cv2.imwrite("C:/LucasRaoni/fontes/workspace/python/innovation/images/pic1.jpg", img))
-    
-    #Save image directly from the url
-    #print(urllib.request.urlretrieve(url, "C:/LucasRaoni/fontes/workspace/python/innovation/images/pic1.jpg"))
+    # Começa a thread que vai fazer as fotos serem tiradas
+    x = threading.Thread(target=thread_function, args=(5,), daemon=True)
+    x.start()
+    x.join()
 
 if __name__ == "__main__":
     main()
-
-def savePhoto(pic):
-    cv2.imwrite("./images/pic1.jpg", pic)
