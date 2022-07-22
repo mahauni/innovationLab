256e3e4f7e14c46f848a7dcd52c188bdedc8f817
Lucas Raoni <lucasraoniha@gmail.com>
2022-07-22 20:25:10 -0300

    more selectors

--- main.py
+++ main.py
@@ -28,15 +28,23 @@ def main():
     input("Type enter to continue the process!")
     
     # Cosmetics selection
-    #inp = input("Want to add hair? (YES) (NO)")
-    #if (inp.upper() == "YES"):
-    #    Hair()
+    inp = input("Want to add hair? (Y) (N)")
+    if (inp.upper() == "Y"):
+        hair = int(input("Which type of hair do you like (1) (2) (3) (4)"))
+        Hair(hair)
+
+    inp = input("Want to add accessories? (Y) (N)")
+    if (inp.upper() == "Y"):
+        accessories = int(input("Which type of hair do you like (1) (2) (3) (4)"))
+        Accessories(accessories)
     
     #input("Type enter to continue the process!")
 
-    # Here the file executed, is the file of saving the 3D image
+    # It makes the pose to take the photo
     os.system("start ./DAZ3D_Scripts/poseTest.duf")
     input("Type enter to continue the process!")
+
+    # Takes the photo of the character
     os.system("start ./DAZ3D_Scripts/takePhoto.dsa")
 ## Find a way to know when the file is done and stop when its not done
 
@@ -67,9 +75,25 @@ def sharpen_photo(j):
     sharpen = cv2.filter2D(image, -1, sharpen_kernel)
     cv2.imwrite("./images/sharpenPic"+str(j)+".jpg", sharpen)
     
-def Hair():
-    os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+def Hair(x):
+    if x == 1:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 2:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 3:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 4:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
 
+def Accessories(x):
+    if x == 1:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 2:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 3:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")
+    elif x == 4:
+        os.system("start ./DAZ3D_Scripts/HairSelect/Armani Hair.duf")    
 
 # Main
 if __name__ == "__main__":
